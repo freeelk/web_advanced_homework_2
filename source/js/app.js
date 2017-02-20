7f412a108755ac14a3370cee8368e176bb5fc67c
Volodymyr <freeelk@gmail.com>
2017-02-20 22:37:07 +0200

    Добавил Google карту на страницу about

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,17 @@
-(function() {
-  'use strict';
+function initMap() {
+  var markerPos = {lat: 50.479144, lng: 34.965221};
+  var center = {lat: 47.589144, lng: 28.965221};
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: center,
+    disableDefaultUI: true,
+    styles: [{"stylers":[{"hue":"#61dac9"},{"saturation":3}]},{"featureType":"water","stylers":[{"color":"#61dac9"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"}]},
+      {featureType:"administrative",elementType:"labels",stylers:[{visibility:"on"}]},{featureType:"road",elementType:"all",stylers:[{visibility:"off"}]},{featureType:"transit",elementType:"all",stylers:[{visibility:"off"}]}]
+  });
+  var marker = new google.maps.Marker({
+    position: markerPos,
+    map: map,
+    icon: "/assets/img/icons/map_marker.png"
+  });
+}
